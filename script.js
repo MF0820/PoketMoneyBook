c9ad790122434a50d273391138d809420916fe25
MF0820 <111821780+MF0820@users.noreply.github.com>
2022-10-15 10:52:26 +0900

    Update script.js

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ let expected;
 
 function test(actual, expected) {
     if (JSON.stringify(actual) === JSON.stringify(expected)) {
-    console.log("Yay! Test PASSED.");
+        console.log("Yay! Test PASSED.");
     } else {
-    console.error("Test FAILED. Keep trying!");
-    console.log("    actual: ", actual);
-    console.log("  expected: ", expected);
-    console.trace();
+        console.error("Test FAILED. Keep trying!");
+        console.log("    actual: ", actual);
+        console.log("  expected: ", expected);
+        console.trace();
     }
 }
 
